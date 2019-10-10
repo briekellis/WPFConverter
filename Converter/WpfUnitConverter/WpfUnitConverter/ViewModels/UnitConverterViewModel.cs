9a26030abeffb388ca90f0c71ac038c56490992e
Brie Ellis <38733065+briekellis@users.noreply.github.com>
2019-10-10 11:12:08 -0700

    Add files via upload

---
+++ Converter/WpfUnitConverter/WpfUnitConverter/ViewModels/UnitConverterViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfUnitConverter.ViewModels
+{
+    
+    public class UnitConverterViewModel : ObservableObject
+    {
+        public ICommand ButtonConvertCommand {get; set;}
+        private double _result;
+
+        public double Operand { get; set; }
+        public string OperandUnit { get; set; }
+
+        public string ResultUnit { get; set; }
+
+        public double Result
+        {
+            get { return _result; }
+            set
+            {
+                _result = value;
+                OnPropertyChanged("Result");
+            }
+        }
+
+        public List<string> Units { get; set; }
+
+        private List<string> BuildOutUnitComboBoxSource()
+        {
+            return new List<string>() { "Inches", "Feet", "Miles", "Millimeters", "Centimeters", "Meters", "Kilometers" };
+        }
+        private void PerformCalculation(object obj)
+        {
+            Result = 55; //value to confirm beinding of command
+        }
+        public UnitConverterViewModel()
+        {
+            Operand = 44; //value to confirm binding of property
+            Units = BuildOutUnitComboBoxSource();
+            ButtonConvertCommand = new RelayCommand(new Action<object>(PerformCalculation));
+        }
+    }
+
+}
+
